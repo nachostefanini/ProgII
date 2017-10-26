4e0d3aaa2df7765d11105b6d6d8fe1f8d48a9372
nachostefanini <ignacio.stefanini@ing.austral.edu.ar>
2017-10-26 12:07:12 -0300

    all

---
+++ Test/AdminMenuTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nachostefanini on 26/10/17.
+ */
+public class AdminMenuTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
